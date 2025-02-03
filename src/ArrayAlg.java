8f6d06bef0a51beb12f33bcdfb2317364e35c684
BLACKHENZ29 <ayehenz29@gmail.com>
2025-02-03 16:35:56 +0100

    2D Algorithm

---
+++ src/ArrayAlg.java
@@ -0,0 +1,28 @@
+//ADD TWO MATRICES TOGETHER
+public class ArrayAlg {
+    public static void main(String[] args) {
+
+        int rows = 2, columns = 3;
+        int[][] firstMatrix = {
+                {2, 3, 4},
+                {5, 2, 3}
+        };
+        int[][] secondMatrix = {
+                {-4, 5, 3},
+                {5, 6, 3}
+        };
+
+        int[][] sum = new int[rows][columns];
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
+        System.out.println("The total of the two matrices is: ");
+        for(int[] row : sum) {
+            for (int column : row) {
+                System.out.print(column + "    " + "\n");
+            }
+        }
+    }
+}
