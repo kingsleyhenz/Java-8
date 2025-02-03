8ddef9f7d3d7538b095856dd003d80eb280acaff
BLACKHENZ29 <ayehenz29@gmail.com>
2025-02-03 16:18:46 +0100

    1D,2D Arrays

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5};
+//        System.out.println(numbers[4]);
+        numbers[4] = 55;
+//        System.out.println(numbers[4]);
+//        for(int i = 0; i < numbers.length; i++){
+//            System.out.println(numbers[i]);
+//        }
+
+//        for(int num : numbers){
+//            System.out.println(num);
+//        }
+
+        int[][] TwoDim = {
+                {1,2,3},
+                {2,3,4},
+                {3,4,5}
+        };
+        System.out.println(TwoDim[0][1]);
+    }
+}
